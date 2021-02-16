8f185c3a281a2608a83e7c228e86ec70c65d7f6e
Léna <lenapancher@gmail.com>
2021-02-16 14:55:30 +0100

    base html + page connexion/inscription

---
+++ connexion.php
@@ -0,0 +1,135 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon super blog</title>
+    <meta charset="utf-8"/>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+<?php
+include("navBar.php");
+?>
+
+<?php
+//
+//try {
+//    $bdd = new PDO('mysql:host=localhost;dbname=espace_membres;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+//} catch (Exception $e) {
+//    die('Erreur : ' . $e->getMessage());
+//}
+//?>
+
+<div class="container d-flex">
+    <div class="incription">
+        <h1 class="text-center font-weight-bold">Inscription</h1>
+        <form class="formInscription">
+            <div class="form-group">
+                <label>Nom</label>
+                <input type="text" class="form-control caseInfoContact" id="lastName" required>
+            </div>
+            <div class="form-group">
+                <label>Prénom</label>
+                <input type="text" class="form-control caseInfoContact" id="firstName" required>
+            </div>
+            <div class="form-group">
+                <label>Adresse Mail</label>
+                <input type="email" class="form-control caseInfoContact" id="email" placeholder="utilisateur@domaine.fr" required>
+            </div>
+            <div class="form-group">
+                <label>Mot de passe</label>
+                <input type="password" class="form-control" name="pass1" id="pass1" required>
+            </div>
+            <div class="form-group">
+                <label>Retapez votre mot de passe</label>
+                <input type="password" class="form-control" name="pass2" id="pass2" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary submit" value="S'inscrire"
+            </div>
+        </form>
+    </div>
+    </div>
+    <div class="connexion">
+        <h1 class="text-center font-weight-bold">Connexion</h1>
+        <form class="formConnexion" action="connexion.php" method="post">
+            <div class="form-group">
+                <label>Adresse Mail</label>
+                <input type="text" class="form-control" name="pseudo" id="email" required placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label>Mot de passe</label>
+                <input type="password" class="form-control" name="pass" id="pass" required placeholder="Mot de passe">
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary submit" value="Se connecter"
+            </div>
+        </form>
+    </div>
+</div>
+
+<?php
+// Inscription
+// Vérification de la validité des informations
+if (isset($_POST['pseudo'], $_POST['pass1'], $_POST['pass2'], $_POST['mail'])) {
+    $pseudo = htmlspecialchars($_POST['pseudo']);
+    $pass1 = $_POST['pass1'];
+    $pass2 = $_POST['pass2'];
+    $mail = htmlspecialchars($_POST['mail']);
+
+    // Vérification du pseudo et du mail
+    $reponse = $bdd->query('SELECT pseudo, email FROM membres WHERE pseudo = "' . $pseudo . '" OR email = "' . $mail . '"');
+    if (!$donnees = $reponse->fetch()) {
+        // Vérification des mots de passe
+        if ($pass1 === $pass2) {
+            // Hachage du mot de passe
+            $pass_hache = password_hash($pass1, PASSWORD_DEFAULT);
+            if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail'])) {
+                // Insertion
+                $req = $bdd->prepare('INSERT INTO membres(pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass1, :mail, CURDATE())');
+                $req->execute(array(
+                    'pseudo' => $pseudo,
+                    'pass1' => $pass_hache,
+                    'mail' => $mail,
+                ));
+                header('Location: connexion.php');
+            } else {
+                echo "L'adresse mail n'est pas valide.";
+            }
+        } else {
+            echo "Les deux mots de passe ne sont pas identiques.";
+        }
+    } else {
+        echo "Le compte est déjà crée, veuillez vous connecter";
+    }
+}
+?>
+
+<?php
+// COnnexion
+//  Récupération de l'utilisateur et de son pass hashé
+if (isset($_POST['pseudo'], $_POST['pass'])) {
+    $pseudo = htmlspecialchars($_POST['pseudo']);
+    $reponse = $bdd->prepare('SELECT id, pass FROM membres WHERE pseudo = :pseudo');
+    $reponse->execute(array(
+        'pseudo' => $pseudo));
+    $resultat = $reponse->fetch();
+
+    // Comparaison du pass envoyé via le formulaire avec la base
+    $isPasswordCorrect = password_verify($_POST['pass'], $resultat['pass']);
+
+    if ($isPasswordCorrect) {
+        $_SESSION['id'] = $resultat['id'];
+        $_SESSION['pseudo'] = $pseudo;
+        header('Location: accueilProfil.php');
+    } else {
+        echo 'Mauvais identifiant ou mot de passe !';
+    }
+}
+
+?>
+
+</body>
+</html>
---
+++ navBar.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site e-commerce</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+
+<header>
+    <!-- Static navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Accueil</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Nos Produits
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="">Tous les articles</a>
+                        <button class="dropdown-item" id="films">Films</button>
+                        <button class="dropdown-item" id="series">Series</button>
+                        <button class="dropdown-item" id="jeux">Jeux</button>
+                        <button class="dropdown-item" id="mangas">Mangas
+                        </button>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="connexion.php">Connexion</a>
+                </li>
+            </ul>
+            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit" data-toggle="modal"
+                    data-target="#exampleModalScrollable">Panier
+            </button>
+        </div>
+    </nav>
+</header>
+
+<!-- /container -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+        crossorigin="anonymous"></script>
+</html>

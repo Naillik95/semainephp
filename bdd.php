ddc02a3a097a9955814919e1d316e999cf1ff949
Léna <lenapancher@gmail.com>
2021-02-16 18:46:54 +0100

    inscription/connexion

--- bdd.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = 'mysql:host=cosql.fakecompany.life;dbname=groupe3;port=3306;charset=utf8';
-    $bdd = new PDO($pdo,'groupe3','Super-Groupe3');
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
-?>
--- connexion.php
+++ connexion.php
@@ -9,7 +9,15 @@
 
 <?php
 include("navBar.php");
-require("bdd.php");
+?>
+
+<?php
+try {
+    $pdo = 'mysql:host=cosql.fakecompany.life;dbname=groupe3;port=3306;charset=utf8';
+    $bdd = new PDO($pdo,'groupe3','Super-Groupe3');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
 ?>
 
 <div class="container d-flex">

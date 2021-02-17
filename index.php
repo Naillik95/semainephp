65b7ed0ccf955639ec8a40d2bcf2a82085269950
Kieran <belli.kieran@gmail.com>
2021-02-17 13:26:59 +0100

    Deconnexion ajouté dans navbar

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-include("navBar.php");
+    include("navBar.php");
+    include("navBarDeco.php");
 ?>
---
+++ navBarDeco.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <title>MicroFactory</title>
+</head>
+
+<body>
+<header>
+    <!-- Static navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Accueil</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Nos Produits
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="">Tous les articles</a>
+                        <button class="dropdown-item" id="films">Films</button>
+                        <button class="dropdown-item" id="series">Series</button>
+                        <button class="dropdown-item" id="jeux">Jeux</button>
+                        <button class="dropdown-item" id="mangas">Mangas</button>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="navBar.php">Déconnexion</a>
+                </li>
+            </ul>
+            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit" data-toggle="modal"
+                    data-target="#exampleModalScrollable">Panier
+            </button>
+        </div>
+    </nav>
+
+    <!-- Modale -->
+    <div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog"
+         aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalScrollableTitle">Mon Panier</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- TESTS POUR LA FENETRE MODALE -->
+                    <section class="container content-section">
+                        <div class="cart-items"></div>
+                        <div class="cart-total"></div>
+                    </section>
+                </div>
+                <div class="modal-footer">
+                    <!-- Pour le total  -->
+                    <div id="totalCount">
+                        <span class="cart-total-title font-weight-bold">Total</span>
+                        <span class="cart-total-price">€</span>
+                    </div>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                    <button class="btn btn-primary btn-purchase" type="button">Acheter</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<!-- /container -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+        crossorigin="anonymous"></script>
+
+</body>
+</html>

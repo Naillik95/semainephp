305221bd3ccda42d9db199ba7c5304669459e40f
Killian <killian.godbertperez@gmail.com>
2021-02-18 14:24:32 +0100

    Automatisation de l'ajout des articles
    Mise en place de l'automatisation totale des articles en PHP

--- all.php
+++ all.php
@@ -4,6 +4,7 @@ require('session.php');
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,130 +15,156 @@ require('session.php');
     <link rel="stylesheet" href="style.css">
     <title>Tous les articles</title>
 </head>
+
 <body>
 
-<h1 class="center">Nos Films</h1>
+    <h1 class="center">Nos Films</h1>
 
-<div class="box">
+
+    <div class="box">
+    <?php
+
+    require_once('bdd.php');
+
+    $query = $bdd->prepare("SELECT p.id, price, name, c.categorie FROM product as p, categorie as c WHERE p.categorie LIKE c.id AND c.categorie LIKE 'film'");
+
+    $query->execute();
+
+    $data = $query->fetchAll();
+
+
+
+    for ($i = 0; $i < count($data); $i++) {
+        $element = $data[$i]["categorie"];
+        $name = $data[$i]["name"];
+        $price = $data[$i]["price"];
+        $id = $data[$i]["id"];
+
+        $replace = str_replace(" ", "_", $name);
+        echo <<<HTML
             <div class="border">
-                <div class="">
-                    <img src="img/the_old_guard.jpg" >
-                    <div class="">
-                        <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
+                <div>
+                    <img src="img/$replace.jpg">
+                    <div>
+                       <a href="cart?id=$id.php" class="button black"> Acheter <i class="fa fa-shopping-cart"></i></a>
                     </div>
-                    <p>The Old Guard<br><b>30€</b></p>
+                    <p>$name<br><b>$price €</b></p>
                 </div>
             </div>
+            HTML;
+    }
 
+    ?>
+    </div>
+    <h1 class="center">Nos Jeux</h1>
+    <div class="box">
+
+    <?php
+
+    $query = $bdd->prepare("SELECT p.id, price, name, c.categorie FROM product as p, categorie as c WHERE p.categorie LIKE c.id AND c.categorie LIKE 'Jeu'");
+
+    $query->execute();
+
+    $data = $query->fetchAll();
+
+
+
+    for ($i = 0; $i < count($data); $i++) {
+        $element = $data[$i]["categorie"];
+        $name = $data[$i]["name"];
+        $price = $data[$i]["price"];
+        $id = $data[$i]["id"];
+
+        $replace = str_replace(" ", "_", $name);
+        echo <<<HTML
             <div class="border">
-                <div class="">
-                    <img src="img/mr_&_mme_Smith.jpg" >
-                    <span class=""></span>
-                    <div class="">
-                        <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
+                <div>
+                    <img src="img/$replace.jpg">
+                    <div>
+                       <a href="cart?id=$id.php" class="button black"> Acheter <i class="fa fa-shopping-cart"></i></a>
                     </div>
+                    <p>$name<br><b>$price €</b></p>
                 </div>
-                <p>Mr & Mme Smith<br><b>30€</b></p>
             </div>
-            <div class="border">
-                <img src="img/mirage.jpg" >
-                <div class="">
-                    <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
+            HTML;
+    }
+
+    ?>
+    </div>  
+    <h1 class="center">Nos Mangas</h1>
+    <div class="box">
+    <?php
+
+$query = $bdd->prepare("SELECT p.id, price, name, c.categorie FROM product as p, categorie as c WHERE p.categorie LIKE c.id AND c.categorie LIKE 'Manga'");
+
+$query->execute();
+
+$data = $query->fetchAll();
+
+
+
+for ($i = 0; $i < count($data); $i++) {
+    $element = $data[$i]["categorie"];
+    $name = $data[$i]["name"];
+    $price = $data[$i]["price"];
+    $id = $data[$i]["id"];
+
+    $replace = str_replace(" ", "_", $name);
+    echo <<<HTML
+        <div class="border">
+            <div>
+                <img src="img/$replace.jpg">
+                <div>
+                   <a href="cart?id=$id.php" class="button black"> Acheter <i class="fa fa-shopping-cart"></i></a>
                 </div>
-                <p>Mirage<br><b>30€</b></p>
+                <p>$name<br><b>$price €</b></p>
             </div>
         </div>
-<h1 class="center">Nos Jeux</h1>
-        <div class="box">
-            <div class="border">
-                <div class="">
-                    <img src="img/Fifa_2021.jpg" >
-                    <div class="">
-                        <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                    <p>FIFA 21<br><b>60€</b></p>
-                </div>
-            </div>
+        HTML;
+}
 
-            <div class="border">
-                <div class="">
-                    <img src="img/smash_bross.jpg" >
-                    <span class=""></span>
-                    <div class="">
-                        <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                </div>
-                <p>Smash Bross<br><b>60€</b></p>
-            </div>
-            <div class="border">
-                <img src="img/Call_of_duty.jpg" >
-                <div class="">
-                    <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
-                </div>
-                <p>Call Of Duty : Modern Warfare<br><b>60€</b></p>
-            </div>
-        </div>
-<h1 class="center">Nos Mangas</h1>
-        <div class="box">
-            <div class="border">
-                <div class="">
-                    <img src="img/one_piece.jpg" >
-                    <div class="">
-                        <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                    <p>One Piece<br><b>7€</b></p>
-                </div>
-            </div>
-
-            <div class="border">
-                <div class="">
-                    <img src="img/attack_on_titant.jpg" >
-                    <span class=""></span>
-                    <div class="">
-                        <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                </div>
-                <p>L'attaque des titans<br><b>8€</b></p>
-            </div>
-            <div class="border">
-                <img src="img/hight_school.jpg" >
-                <div class="">
-                    <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
-                </div>
-                <p>High School of the Dead<br><b>9€</b></p>
-            </div>
-        </div>
-<h1 class="center">Nos Séries</h1>
-        <div class="box">
-            <div class="border">
-                <div class="">
-                    <img src="img/the_walking_dead.jpg" >
-                    <div class="">
-                        <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                    <p>The Walking Dead<br><b>30€</b></p>
-                </div>
-            </div>
-
-            <div class="border">
                 <div class="">
                     <img src="img/le_jeu_de_la_dame.jpg" >
                     <span class=""></span>
                     <div class="">
-                        <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
-                    </div>
+?>
+    </div>   
+    <h1 class="center">Nos Séries</h1>
+    <div class="box">
+    <?php
+
+$query = $bdd->prepare("SELECT p.id, price, name, c.categorie FROM product as p, categorie as c WHERE p.categorie LIKE c.id AND c.categorie LIKE 'Serie'");
+$query->execute();
+
+$data = $query->fetchAll();
+
+
+
+for ($i = 0; $i < count($data); $i++) {
+    $element = $data[$i]["categorie"];
+    $name = $data[$i]["name"];
+    $price = $data[$i]["price"];
+    $id = $data[$i]["id"];
+
+    $replace = str_replace(" ", "_", $name);
+    echo <<<HTML
+        <div class="border">
+            <div>
+                <img src="img/$replace.jpg">
+                <div>
+                   <a href="cart?id=$id.php" class="button black"> Acheter <i class="fa fa-shopping-cart"></i></a>
                 </div>
-                <p>Le jeu de la dame<br><b>10€</b></p>
-            </div>
-            <div class="border">
-                <img src="img/the_100.jpg" >
-                <div class="">
-                    <button class="button black"> Acheter <i class="fa fa-shopping-cart"></i></button>
-                </div>
-                <p>The 100<br><b>50€</b></p>
+                <p>$name<br><b>$price €</b></p>
             </div>
         </div>
+        HTML;
+}
 
-    
+?>
+        
+    </div>
+
+
 </body>
+
 </html>
--- navBar.php
+++ navBar.php
@@ -13,12 +13,14 @@
 
 <body>
 <header>
+   
     <!-- Static navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
@@ -30,12 +32,28 @@
                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Nos Produits
                     </a>
+                    
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="all.php">Tous les articles</a>
-                        <a class="dropdown-item" href="moovies.php">Films</a>
-                        <a class="dropdown-item" href="series.php">Series</a>
-                        <a class="dropdown-item" href="games.php">Jeux</a>
-                        <a class="dropdown-item" href="mangas.php">Mangas</a>
+
+                        <?php
+
+                        require_once('bdd.php');
+
+                        $query = $bdd->prepare("SELECT categorie FROM categorie");
+
+                        $query->execute();
+
+                        $data = $query->fetchAll();
+
+                        for ($i = 0; $i < count($data); $i++) {
+                            $element = $data[$i]["categorie"];
+                            echo "<a class='dropdown-item' href='$element.php'>$element</a>";
+                        }
+
+                        ?>
+
+
                     </div>
                 </li>
                 <li class="nav-item">
